414a4aa8f0aebaaefaab4a381a039c52555bc4ff
Nguyen Thanh Phuong Thao <86770002+thaorngnx@users.noreply.github.com>
2024-06-22 14:40:49 +0700

    update socket url

--- socket.js
+++ socket.js
@@ -1,8 +1,6 @@
 import { io } from 'socket.io-client';
 
-// const URL = process.env.SOCKET_URL;
-const URL =
-  'https://petshop-api-f5ef07c9f712.herokuapp.com';
+const URL = process.env.SOCKET_URL;
 
 export const socket = (token) => {
   return io(URL, { path: '/socket', query: { token } });
